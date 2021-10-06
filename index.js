60858a5b334be8536245e6e88eee58936d27619c
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-06 15:54:46 -0400

    tech section done

---
+++ index.js
@@ -0,0 +1,10 @@
+const secTop = document.getElementById('first-slide');
+const secBottom = document.getElementById('white-tech-box');
+
+secTop 
+
+function hoverApart (el) {
+    el.addEventListener("mouseover", function () {
+        
+    })
+}
