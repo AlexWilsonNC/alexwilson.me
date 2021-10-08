589e4b5225323dd0113acce07ba93b876d6342ff
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-08 10:13:57 -0400

    dark mode transition and resume download btn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.getElementById('theme-btn').addEventListener('click', function () {
     const button = document.getElementById('theme-btn');
+    const pWhite = document.querySelectorAll('.project-text');
 
     document.body.classList = 'dark';
     document.getElementById('nav').style.backgroundColor = 'black';
@@ -7,22 +8,23 @@ document.getElementById('theme-btn').addEventListener('click', function () {
     document.getElementById('white-tech-box').style.color = 'white';
     document.getElementById('testimonials').style.color = 'white';
     document.getElementById('up-arrow').style.color = 'black';
+    // pWhite.style.color = 'white'; //////////////////////
     button.style.color = 'black';
     button.style.backgroundColor = 'white';
     button.innerText = 'Light Mode';
     button.addClass('lightSwitch');
 });
 
-document.getElementsByClassName('lightSwitch').addEventListener('click', function () {
-    const button = document.getElementById('theme-btn');
+// document.getElementsByClassName('lightSwitch').addEventListener('click', function () {
+//     const button = document.getElementById('theme-btn');
 
-    document.body.classList = '';
-    document.getElementById('nav').style.backgroundColor = 'white';
-    document.getElementById('white-tech-box').style.backgroundColor = 'white';
-    document.getElementById('white-tech-box').style.color = 'black';
-    document.getElementById('testimonials').style.color = 'black';
-    document.getElementById('up-arrow').style.color = 'white';
-    button.style.color = 'white';
-    button.style.backgroundColor = 'black';
-    button.innerText = 'Dark Mode';
-});
+//     document.body.classList = '';
+//     document.getElementById('nav').style.backgroundColor = 'white';
+//     document.getElementById('white-tech-box').style.backgroundColor = 'white';
+//     document.getElementById('white-tech-box').style.color = 'black';
+//     document.getElementById('testimonials').style.color = 'black';
+//     document.getElementById('up-arrow').style.color = 'white';
+//     button.style.color = 'white';
+//     button.style.backgroundColor = 'black';
+//     button.innerText = 'Dark Mode';
+// });
