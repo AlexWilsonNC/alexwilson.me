c8745cdcca1f10a94c98c2900fdd8796bf1d2139
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-08 01:17:56 -0400

    dark mode theme switch added

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const secTop = document.getElementById('first-slide');
-const secBottom = document.getElementById('white-tech-box');
+document.getElementById('theme-btn').addEventListener('click', function () {
+    const button = document.getElementById('theme-btn');
 
-secTop 
+    document.body.classList = 'dark';
+    document.getElementById('nav').style.backgroundColor = 'black';
+    document.getElementById('white-tech-box').style.backgroundColor = 'rgb(22, 22, 22)';
+    document.getElementById('white-tech-box').style.color = 'white';
+    document.getElementById('testimonials').style.color = 'white';
+    document.getElementById('up-arrow').style.color = 'black';
+    button.style.color = 'black';
+    button.style.backgroundColor = 'white';
+    button.innerText = 'Light Mode';
+    button.addClass('lightSwitch');
+});
 
-function hoverApart (el) {
-    el.addEventListener("mouseover", function () {
-        
-    })
-}
+document.getElementsByClassName('lightSwitch').addEventListener('click', function () {
+    const button = document.getElementById('theme-btn');
+
+    document.body.classList = '';
+    document.getElementById('nav').style.backgroundColor = 'white';
+    document.getElementById('white-tech-box').style.backgroundColor = 'white';
+    document.getElementById('white-tech-box').style.color = 'black';
+    document.getElementById('testimonials').style.color = 'black';
+    document.getElementById('up-arrow').style.color = 'white';
+    button.style.color = 'white';
+    button.style.backgroundColor = 'black';
+    button.innerText = 'Dark Mode';
+});
