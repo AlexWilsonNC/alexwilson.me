36a137cca3db890e06fbfd2adbe1e9df0dfa2b08
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-09 17:49:13 -0400

    vampire profile pic

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-document.getElementById('theme-btn').addEventListener('click', function () {
-    const button = document.getElementById('theme-btn');
-    const pWhite = document.querySelectorAll('.project-text');
-
-    document.body.classList = 'dark';
-    document.getElementById('nav').style.backgroundColor = 'black';
-    document.getElementById('white-tech-box').style.backgroundColor = 'rgb(22, 22, 22)';
-    document.getElementById('white-tech-box').style.color = 'white';
-    document.getElementById('testimonials').style.color = 'white';
-    document.getElementById('up-arrow').style.color = 'black';
-    // pWhite.style.color = 'white'; //////////////////////
-    button.style.color = 'black';
-    button.style.backgroundColor = 'white';
-    button.innerText = 'Light Mode';
-    button.addClass('lightSwitch');
-});
+// document.getElementById('theme-btn').addEventListener('click', function () {
+// });
 
 // document.getElementsByClassName('lightSwitch').addEventListener('click', function () {
 //     const button = document.getElementById('theme-btn');
@@ -28,3 +14,8 @@ document.getElementById('theme-btn').addEventListener('click', function () {
 //     button.style.backgroundColor = 'black';
 //     button.innerText = 'Dark Mode';
 // });
+
+document.getElementById('theme-btn').addEventListener('click', function () {
+
+  document.body.classList = 'dark';
+});
