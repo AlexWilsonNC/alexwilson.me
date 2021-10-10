48bd06bdcc1a72bc84d9055958d6856de2cb949f
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-10-10 00:11:37 -0400

    space

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ document.getElementById('theme-btn').addEventListener('click', function () {
 
   }
 });
+
+document.getElementById('theme-btn-space').addEventListener('click', function () {
+  if (document.body.classList == '') {
+    document.body.classList.add('space')
+    document.getElementById('theme-btn-space').innerText = 'Main Theme';
+  } else {
+    document.body.classList.remove('space')
+    document.getElementById('theme-btn-space').innerText = 'Outer Space';
+
+  }
+});
